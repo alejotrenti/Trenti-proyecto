759e45bce3737c4b1386b909454efee111a590e2
alejo <163149710+alejotrenti@users.noreply.github.com>
2024-06-25 15:57:39 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Log In</title>
+    <title>Log In</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="style.css">
-
+    <link rel="stylesheet" type="text/css" href="style.css">
+ 
 </head>
 <body>
     <div class="background">
@@ -19,21 +19,29 @@
     <form action="login2.php" method="post">
         <h3>Log In</h3>
         <label for="username">Usuario</label>
-    	<input id="username" type="text" name="name" placeholder="Usuario">
-
+        <input id="username" type="text" name="name" placeholder="Usuario">
+ 
         <label for="password">Contraseña</label>
-    	<input id="password" type="password" name="contra" placeholder="Contraseña">
-    	
+        <input id="password" type="password" name="contra" placeholder="Contraseña">
+       
         <input type="submit" name="register" >
-
+ 
         <div class="social">
-            <a style="  text-decoration: none;" href=".//login_google/index.php"><div class="go"><i class="fab fa-google" ></i> Google</div></a>
-        </div>  
+         
+            <?php require ('autentification.php')?>
+            <a  style="  text-decoration: none;" href="<?php echo $client->createAuthUrl() ?>">
+                <div class="go">
+                    <i class="fab fa-google"></i>
+                    Iniciar sesión con Google
+                </div>
+            </a>
+            <script src="validar.js"></script>
+         </div>
         <br>
             <a href="registrarse.php" class="button">Crear usuario</a><br>
             <a href="newpassword.php" class="button">Olvido su contraseña?</a><br>
     </form>
-
-	
+ 
+   
 </body>
 </html>
