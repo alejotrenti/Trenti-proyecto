65ac15cf4b67d4d69c5d68d05213642422df3de3
alejo <163149710+alejotrenti@users.noreply.github.com>
2024-06-25 17:00:16 -0300

    Update login2.php

--- login2.php
+++ login2.php
@@ -1,4 +1,5 @@
 <?php
+session_start(); 
 $conex = mysqli_connect("localhost", "root", "", "registro"); 
 
 $usuario = $_POST['name'];
@@ -12,33 +13,55 @@ if (mysqli_num_rows($result) == 1) {
     $hashedPassword = $row['contra'];
 
     if (password_verify($password, $hashedPassword)) {
-        session_start();
         $_SESSION["nombre"] = $usuario;
         header("Location: success.php");
+        exit; 
     } else {
-        ?> 
+        ?>
+        <!DOCTYPE html>
         <html>
-
         <head>
             <link rel="preconnect" href="https://fonts.gstatic.com">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
             <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
             <link rel="stylesheet" type="text/css" href="style.css">
         </head>
+        <body>
             <form>
                 <div>
                     <h1>Contraseña incorrecta</h1>
                     <h3>Pruebe de nuevo</h3>
-                    <a align="center" href="index.php">Volver</a>
+                    <a href="index.php">Volver</a>
                 </div>
             </form>
+        </body>
         </html>
-        <?php   
-           
-
+        <?php
+        exit; 
     }
-COMPLETE
-} 
+
+} else {
+    ?>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <form>
+            <div>
+                <h1>Usuario no encontrado</h1>
+                <h3>Verifique el nombre de usuario</h3>
+                <a href="index.php">Volver</a>
+            </div>
+        </form>
+    </body>
+    </html>
+    <?php
+}
 
 mysqli_close($conex);
 ?>
